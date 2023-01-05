fe307ac0ee08291b33a51964f61fc6fd98a522d8
VictorOlof <victorolofsson@hotmail.com>
2023-01-05 16:41:06 +0100

    test

--- src/About.js
+++ src/About.js
@@ -6,52 +6,79 @@ class About extends Component {
     render() {
     return (
     <div>
-    <h1>
-        Om oss
-    </h1>
-    <div className="about-us">
-    <ul>
-    <p>Trafiklärarna på Körkarlens trafikskola är välutbildade och duktiga på sina arbetsuppgifter.</p> 
-        <p>Vi har x lärare, Vi har x fordon, med x erfarenhet och funnits sedan 2005</p>
-        <p>Vi erbjuder på Körkarlens trafikskola:</p><br></br>
-        
-        <br></br>
-        <div className="about-li">
-            <li>✓ Alltid mycket låga och konkurrenskraftiga priser</li>
-            <li>✓ Hög kvalitet i undervisningen</li>'<br></br><br></br>
-            <li>✓ En trevlig och avspänd atmosfär på trafikskolan, vilket skapar en bra miljö för en god inlärning</li><br></br><br></br>
-            <li>✓ Vi har även ADR utbildningar.</li><br></br><br></br>
-            <li>✓ Vi erbjuder även utbildning för Yrkesförarkompetens (YKB)</li><br></br><br></br>
-        </div>
-        
-    </ul>
-    </div>
-
-
+    <h1>Om oss</h1>
 
     <h2 className="center-h">Vilka är vi?</h2>
         <p>Körkarlens Trafikskola är en enskild firma vars verksamhet är trafikskola med körlektioner för personbil,
             lastbil,lastbil med släp,bil med släp och buss och teori.
              Ykb-utbildningar och ADR.</p>
-             <p>Kom gärna till kontoret till Sägengatan och besök oss!</p>
+        <p>Kom gärna till kontoret till Sägengatan och besök oss!</p>
 
-    
     <h2 className="center-h">Har du frågor?</h2>
     <div className="center h">
         <br></br>
-        
-                <div className="container-about">
-                    <img src={require('./components/images/car-images/IMG_FRAMSIDA.JPG')} className="rounded-circle mx-auto d-block" alt="front door"/>
-                </div>
-            
-           
-                                        
+        <div className="container-about">
+            <img src={require('./components/images/car-images/IMG_FRAMSIDA.JPG')} className="rounded-circle mx-auto d-block" alt="front door"/>
+            <p className="center-h">Besök oss på Sägengatan 31, eller ring gärna oss på 0700-90 72 10 eller <a href="mailto:korkarlens.trafikskola@hotmail.com">mail</a></p>
+        </div> 
+    </div>
+        <h2 className="center-h">Våra fordon</h2>
+        <div className="image-container">
+            <p>Vi använder oss av ett flertal nya volkswagen för personbil och har diverse tyngre fordon för övriga utbildningar</p>
+            <div className="gallery">
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/IMG_6871.JPG')} className="gallery-image" alt="cars"/>                    
 
                 </div>
-    <p className="center-h">Besök oss på Sägengatan 31, eller ring gärna oss på 0700-90 72 10 eller <a href="mailto:korkarlens.trafikskola@hotmail.com">mail</a>
-    </p>
-    
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/IMG_6881.JPG')} className="gallery-image" alt="cars"/>                    
+
+                </div>
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/IMG_6888.JPG')} className="gallery-image" alt="cars"/>                    
+
+                </div>
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/IMG_6890.JPG')} className="gallery-image" alt="cars"/>                    
+
+                </div>
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/IMG_6891.JPG')} className="gallery-image" alt="cars"/>                    
+
+                </div>
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/IMG_6892.JPG')} className="gallery-image" alt="cars"/>                    
+
+                </div>
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/IMG_6895.JPG')} className="gallery-image" alt="cars"/>                    
+
+                </div>
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/IMG_6897.JPG')} className="gallery-image" alt="cars"/>                    
+
+                </div>
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/Vit automat 2.jpg')} className="gallery-image" alt="cars"/>                    
+
+                </div>
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/IMG_6903.JPG')} className="gallery-image" alt="cars"/>                    
+
+                </div>
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/IMG_FORDON1.jpg')} className="gallery-image" alt="cars"/>                    
+
+                </div>
+                <div className="gallery-item">
+                    <img src={require('./components/images/car-images/IMG_6901.JPG')} className="gallery-image" alt="cars"/>                    
+
+                </div>
+            </div>
+        </div>
     </div>
+
+    
     );
     }
 }
--- src/Home.js
+++ src/Home.js
@@ -7,88 +7,49 @@ class Home extends Component {
     return (
         
       <div>
-     
         <div className="home-text">
             
-        <h2 className="center-h">Information</h2>
-        <h3 className="center-h">Jul Erbjudande </h3>
-        <p className="lh-base">
-            5 körlektioner för 2250 kr ( ink vägmärkesbok)
-            10 körlektioner för 4300 kr ( ett körhäfte och vägmärkesboken)
-            Intensiv paketet 20 körlektioner för 8700 kr ( körkortsboken och vägmärkesboken och teoritester från datan)
-            Gäller från 1 december, 31 januari 2023. 
-        </p>
-        <p className="lh-base">Förövrigt så vill vi informera att vi har gott om tider till körlektioner personbil, mellan kl 08.30-16.50 ring oss gärna på 0700-907210 eller maila dagar och tider önskemål till korkarlens.
-        trafikskola@hotmail.com. Maila eller ring oss för frågor om exempelvis pris.</p>
-        <hr></hr>
-        <div>
-            <h2>Våra utbildningar</h2>
-            <Tabbed/>
-        </div>
-        <hr></hr>
-    <h2 className="center-h">Våra fordon</h2>
-    </div>
-    
-        <div className="image-container">
-            <p>Vi använder oss av ett flertal nya volkswagen för personbil och har diverse tyngre fordon för övriga utbildningar</p>
-            <div className="gallery">
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/IMG_6871.JPG')} className="gallery-image" alt="cars"/>                    
+            <h2 className="center-h">Information</h2>
+            <h3 className="center-h">Jul Erbjudande </h3>
+            <p className="lh-base">
+                5 körlektioner för 2250 kr ( ink vägmärkesbok)
+                10 körlektioner för 4300 kr ( ett körhäfte och vägmärkesboken)
+                Intensiv paketet 20 körlektioner för 8700 kr ( körkortsboken och vägmärkesboken och teoritester från datan)
+                Gäller från 1 december, 31 januari 2023. 
+            </p>
+            <p className="lh-base">
+                Förövrigt så vill vi informera att vi har gott om tider till körlektioner personbil, mellan kl 08.30-16.50 ring oss gärna på 0700-907210 eller maila dagar och tider önskemål till korkarlens.
+                trafikskola@hotmail.com. Maila eller ring oss för frågor om exempelvis pris.
+            </p>
+            
+            <hr></hr>
 
+            <div className="">
+            <ul>
+            <p>Trafiklärarna på Körkarlens trafikskola är välutbildade och duktiga på sina arbetsuppgifter.</p> 
+                <p>Vi har x lärare, Vi har x fordon, med x erfarenhet och funnits sedan 2005</p>
+                <p>Vi erbjuder på Körkarlens trafikskola:</p><br></br>
+                
+                <br></br>
+                <div className="about-li">
+                    <li>✓ Alltid mycket låga och konkurrenskraftiga priser</li>
+                    <li>✓ Hög kvalitet i undervisningen</li>'<br></br><br></br>
+                    <li>✓ En trevlig och avspänd atmosfär på trafikskolan, vilket skapar en bra miljö för en god inlärning</li><br></br><br></br>
+                    <li>✓ Vi har även ADR utbildningar.</li><br></br><br></br>
+                    <li>✓ Vi erbjuder även utbildning för Yrkesförarkompetens (YKB)</li><br></br><br></br>
                 </div>
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/IMG_6881.JPG')} className="gallery-image" alt="cars"/>                    
-
-                </div>
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/IMG_6888.JPG')} className="gallery-image" alt="cars"/>                    
-
-                </div>
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/IMG_6890.JPG')} className="gallery-image" alt="cars"/>                    
-
-                </div>
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/IMG_6891.JPG')} className="gallery-image" alt="cars"/>                    
-
-                </div>
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/IMG_6892.JPG')} className="gallery-image" alt="cars"/>                    
-
-                </div>
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/IMG_6895.JPG')} className="gallery-image" alt="cars"/>                    
-
-                </div>
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/IMG_6897.JPG')} className="gallery-image" alt="cars"/>                    
-
-                </div>
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/Vit automat 2.jpg')} className="gallery-image" alt="cars"/>                    
-
-                </div>
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/IMG_6903.JPG')} className="gallery-image" alt="cars"/>                    
-
-                </div>
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/IMG_FORDON1.jpg')} className="gallery-image" alt="cars"/>                    
-
-                </div>
-                <div className="gallery-item">
-                    <img src={require('./components/images/car-images/IMG_6901.JPG')} className="gallery-image" alt="cars"/>                    
-
-                </div>
-
+                
+            </ul>
             </div>
+
+            <hr></hr>
+            <div>
+                <h2>Våra utbildningar</h2>
+                <Tabbed/>
+            </div>
+
+            <hr></hr>
         </div>
-        
-
-
-        
-        
-        
     </div>
     );
     }
